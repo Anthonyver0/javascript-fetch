559f5288de1bf4a4aa4f8867b5ffacc376db11dd
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2021-11-04 10:20:10 +0100

    ej01 planteado e iniciado

--- ej01-buscaminas/scripts/ej01-prof.js
+++ ej01-buscaminas/scripts/ej01-prof.js
@@ -1,3 +1,23 @@
 (function(){
+    const WIDTH = 9
+    const HEIGHT = 9
+    const board = document.querySelector("#tablero")
+    const fila = document.querySelector("#fila")
+    const columna = document.querySelector("#columna")
 
+    for (let i = 0; i < WIDTH * HEIGHT; i++) {
+        // i = 0 , 1, 2, 3, 4 ... 80
+        let newCell = document.createElement("DIV")
+        newCell.classList.add("celda")
+        newCell.dataset.fila = Math.floor(i / WIDTH)
+        newCell.dataset.columna = i % WIDTH
+        board.append(newCell)
+    }
+
+    board.addEventListener("mouseover",(ev)=>{
+        if (ev.target.classList.contains("celda")) {
+            fila.textContent = ev.target.dataset.fila
+            columna.textContent = ev.target.dataset.columna
+        }
+    })
 })()
