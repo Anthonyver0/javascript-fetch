dfd0997864dd65ff7e0f77c7bc2b6b2af6309deb
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2021-11-12 10:24:00 +0100

    terminada expansión

--- ej01-buscaminas/scripts/ej01-prof.js
+++ ej01-buscaminas/scripts/ej01-prof.js
@@ -2,18 +2,25 @@
     const WIDTH = 9
     const HEIGHT = 9
     const MINES = 10
+    const reloj = document.querySelector("#reloj")
     const board = document.querySelector("#tablero")
     const fila = document.querySelector("#fila")
     const columna = document.querySelector("#columna")
     const mina = document.querySelector("#mina")
     const empezar = document.querySelector("#empezar")
     let partidaEnJuego = false
+    let timer
 
     setupMouse()
     empezar.addEventListener("click",function(){
         generateBoard()
         assignMines()
         partidaEnJuego = true
+        reloj.textContent = "0"
+        clearInterval(timer)
+        timer = setInterval(function(){
+            reloj.textContent++
+        },1000)
     })
 
     function setupMouse() {
@@ -30,20 +37,26 @@
             //partida en marcha: analizar todos los posibles casos
             if (ev.button == 0) {
                 //se ha pulsado el botón izquierdo
-                ev.target.dataset.clicada = true
                 if (ev.target.dataset.mina == "true") {
                     //has clicado mina -> pierdes
                     partidaEnJuego = false
                     descubrirMinas()
                     ev.target.classList.add("mina_explotada")
+                    clearInterval(timer)
                 } else {
                     //no has clicado mina
                     //calcular número minas alrededor
                     let minesAround = calculateMinesAround(parseInt(ev.target.dataset.fila),
                                                            parseInt(ev.target.dataset.columna))
                     ev.target.classList.add("celda_clicada"+minesAround)
+                    //expandir descubrimiento de celdas
+                    if (!minesAround)
+                        expand(parseInt(ev.target.dataset.fila),parseInt(ev.target.dataset.columna))
+                    ev.target.dataset.clicada = true
                     //comprobar si con este último clic has ganado
-
+                    let numClicadas = document.querySelectorAll(".celda[data-clicada=true]").length
+                    if (numClicadas == (WIDTH*HEIGHT-MINES))
+                        victory()
                 }
             } else if (ev.button == 2) {
                 //se ha pulsado el botón derecho
@@ -101,5 +114,51 @@
         }
         return around
     } // fin de la función "calculateMinesAround"
+
+    function expand(fila,col) {
+        console.log("expand: entramos")
+        let celda = document.querySelector(`.celda[data-fila='${fila}'][data-columna='${col}']`)
+        let minesAround = calculateMinesAround(fila,col)
+        if (celda.dataset.clicada == "true")
+            return
+        else {
+            celda.dataset.clicada = true
+            celda.classList.add("celda_clicada"+minesAround)
+        }
+        if (minesAround > 0) 
+            return
+        
+        for (let i=fila-1; i<=fila+1; i++) {
+            for (let j=col-1; j<=col+1; j++) {
+                celda = document.querySelector(`.celda[data-fila='${i}'][data-columna='${j}']`)
+                if (celda) expand(i,j)
+            }
+        }
+        /*
+        //expandir arriba
+        console.log("expand: iniciamos IFs de expansión")
+        if (fila > 0)
+            expand(fila-1,col)
+        //expandir abajo
+        if (fila < HEIGHT-1)
+            expand(fila+1,col)
+        //expandir izq
+        if (col > 0)
+            expand(fila,col-1)
+        //expandir der
+        if (col < WIDTH-1)
+            expand(fila,col+1)
+            */
+
+    }
+
+    function victory() {
+        partidaEnJuego = false
+        clearInterval(timer)
+        descubrirMinas()
+        //mostrar tabla de mejores puntuaciones
+        //comprobar si la nueva puntuación merece entrar
+        alert("asd")
+    }
 
 })() // fin de nuestro código encapsulado
