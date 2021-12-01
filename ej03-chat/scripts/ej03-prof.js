dd2e74e3e775613c924b1802cb9a9d162d692d58
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2021-12-01 11:00:51 +0100

    ej03 terminado

--- ej03-chat/scripts/ej03-prof.js
+++ ej03-chat/scripts/ej03-prof.js
@@ -8,6 +8,30 @@
 
     refresco = setInterval(recargarMensajes,1000)
 
+    teclado.addEventListener('keyup',function(ev){
+        if (ev.key == "Enter") {
+            insertarMensaje()
+        }
+    })
+
+    enviar.addEventListener("click",function(ev){
+        insertarMensaje()
+    })
+
+    function insertarMensaje(){
+        if (nick.value.trim().length && teclado.value.trim().length) {
+            let params = new URLSearchParams(`nick=${nick.value.trim()}&texto=${teclado.value.trim()}`)
+            //let params = new URLSearchParams("nick="+nick.value.trim()+"&texto="+teclado.value.trim())
+            let options = {
+                method: "POST",
+                body: params
+            }
+            fetch('server/chat_insert_post.php',options)
+            //vaciar el INPUT teclado
+            teclado.value = ""
+        }
+    }
+
     function recargarMensajes() {
         fetch('server/chat_select_get_xml.php?ultimo='+ultimoMensaje)
         .then(response=>response.text())
@@ -16,7 +40,6 @@
             let xml = parser.parseFromString(xmlCrudo,'text/xml')
             let mensajes = xml.querySelectorAll("mensaje")
             mensajes.forEach(msj=>{
-
                 let id = msj.querySelector("id").textContent
                 let nick = msj.querySelector("nick").textContent
                 let texto = msj.querySelector("texto").textContent
@@ -28,8 +51,11 @@
                 chat.append(newP)
 
                 ultimoMensaje = id
+                //bajar el scroll del chat hasta abajo para que se vean los nuevos
+                chat.scrollTop = chat.scrollHeight
             })
         })
     }
+
 
 })()
