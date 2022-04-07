88408960c5269000cd41a7f52ca585f06bd71c90
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2022-04-07 10:05:13 +0200

    ej6 casi terminado

--- ej06-ev/scripts/ev.js
+++ ej06-ev/scripts/ev.js
@@ -10,13 +10,31 @@ lis.forEach( (li,index)=>{
         .then( respcruda => respcruda.json() )
         .then( json => {
             //recorrer cada resultado de búsqueda para mostrarlo en pantalla
-            document.querySelector("#results").innerHTML = "<UL></UL>"
+            document.querySelector("#results").innerHTML = '<UL class="list-group"></UL>'
             let ul = document.querySelector("#results>UL")
             json.forEach( result => {
                 //añadir ese result a la lista UL
                 // ese result puede ser una tecno, un fabr o un coche
                 let newLI = document.createElement("LI")
-                newLI.textContent = result
+                newLI.classList.add("list-group-item")
+                newLI.dataset.toggle="modal"
+                newLI.dataset.target="#carThumb"
+                //distinguir si es coche o si es tecn/fabr
+                newLI.textContent = result.text ? result.text : result.nombre + " " + result.precio + "€"
+                newLI.addEventListener("mouseover",ev=>{
+                    if (document.querySelector(".active"))
+                        document.querySelector(".active").classList.remove("active")
+                    newLI.classList.add("active")
+                })
+                newLI.addEventListener("click",ev=>{
+                    //distinguir si es coche o no
+                    if (!result.text) {
+                       const modal = document.querySelector("div.modal-dialog")
+                       const imagen = modal.querySelector("img")
+                       imagen.src = result.imagen
+                       $('#carThumb').modal('show');
+                    }
+                })
                 ul.append(newLI)
             })
         })
